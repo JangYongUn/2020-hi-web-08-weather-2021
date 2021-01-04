34d5d410b0ebac38de2eaf7eef9bdf39d0d59679
JangYongUn <sm970413@naver.com>
2021-01-04 13:16:34 +0900

    ....

--- public/js/index.js
+++ public/js/index.js
@@ -8,3 +8,7 @@
 
 /********* 이벤트선언 **********/
 
+// openweather app : 7e9fe622cfa1cb7e7399447de68c392f    (본인거로)
+// kakao developers map : cd85347e3ff4d4312dd4f3f9d775ce8a    (본인거로)
+
+// openweather app 7days : http://api.openweathermap.org/data/2.5/onecall?lat=38&lon=127&appid=7e9fe622cfa1cb7e7399447de68c392f&units=metric&exclude=minutely,hourly
